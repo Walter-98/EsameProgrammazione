a6f25fa6c0070b36a80e77cb03bc354cab0eceb9
Walter-98 <75211109+Walter-98@users.noreply.github.com>
2021-01-28 15:59:41 +0100

    Add files via upload

---
+++ TicketMaster/src/test/java/com/esameOOP/TicketMaster/TicketMasterApplicationTests.java
@@ -0,0 +1,13 @@
+package com.esameOOP.TicketMaster;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TicketMasterApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
